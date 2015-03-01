f09e0a514352fbf630941da4b12e9f7551ba9bf3
Lisa Vogt <lisavogtsf@users.noreply.github.com>
2015-03-01 11:07:26 -0800

    brings in trips and bios table into schema

--- immigration_tracker/db/schema.rb
+++ immigration_tracker/db/schema.rb
@@ -11,16 +11,38 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150228231901) do
+ActiveRecord::Schema.define(version: 20150301030046) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "users", force: :cascade do |t|
-    t.string   "login_name"
-    t.string   "password"
+  create_table "bios", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.date     "dob"
+    t.string   "citizenship"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+    t.string   "place_of_birth"
+    t.integer  "user_id"
+  end
+
+  create_table "trips", force: :cascade do |t|
+    t.date     "start_date"
+    t.date     "end_date"
+    t.string   "country"
+    t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "email"
+    t.string   "password"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
